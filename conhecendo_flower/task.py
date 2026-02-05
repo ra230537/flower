c2b337fe50ec26990f3a454e04a50e32567e9ba7
Julio Oliveira <julio.oliveira@amlabs.com.br>
2026-02-05 08:33:22 -0300

    feat: add comments to explain task file

--- conhecendo_flower/task.py
+++ conhecendo_flower/task.py
@@ -32,6 +32,10 @@ class Net(nn.Module):
 
 fds = None  # Cache FederatedDataset
 
+# Normalize: normalizes each rgb channel using the formula (tensor - mean) / std
+# ToTensor: converts a PIL Image or numpy.ndarray to tensor
+# Componse: chains multiple transforms together
+# Pytorch takes raw images, converts them to tensors, and normalizes them
 pytorch_transforms = Compose([ToTensor(), Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 
@@ -42,20 +46,26 @@ def apply_transforms(batch):
 
 
 def load_data(partition_id: int, num_partitions: int):
-    """Load partition CIFAR10 data."""
+    """Load partition CIFAR10 data. -> images divided into 10 classes 
+    (airplane, automobile, bird, cat, deer, dog, frog, horse, ship, truck)"""
     # Only initialize `FederatedDataset` once
     global fds
     if fds is None:
+        # IidPartitioner: Strategy to explain how to partition the data
+        # The partitions are independent and identically distributed (IID)
         partitioner = IidPartitioner(num_partitions=num_partitions)
         fds = FederatedDataset(
             dataset="uoft-cs/cifar10",
             partitioners={"train": partitioner},
         )
+    # Each client loads its own partition
+    # Its used in cross validation techniques
     partition = fds.load_partition(partition_id)
     # Divide data on each node: 80% train, 20% test
     partition_train_test = partition.train_test_split(test_size=0.2, seed=42)
     # Construct dataloaders
     partition_train_test = partition_train_test.with_transform(apply_transforms)
+    #shuffle=True: the data is reshuffled at every epoch
     trainloader = DataLoader(partition_train_test["train"], batch_size=32, shuffle=True)
     testloader = DataLoader(partition_train_test["test"], batch_size=32)
     return trainloader, testloader
@@ -67,15 +77,23 @@ def train(net, trainloader, epochs, lr, device):
     criterion = torch.nn.CrossEntropyLoss().to(device)
     optimizer = torch.optim.Adam(net.parameters(), lr=lr)
     net.train()
-    running_loss = 0.0
     for _ in range(epochs):
+        # I put running loss inside the epoch loop to reset it every epoch
+        running_loss = 0.0
         for batch in trainloader:
             images = batch["img"].to(device)
             labels = batch["label"].to(device)
+            # Clear the gradients
             optimizer.zero_grad()
+            # Forward pass: predict and compute loss
+            # The loss are computed using the criterion function, we have the labels and the predictions
+            # net(images) gives us predictions and labels are the true results.
             loss = criterion(net(images), labels)
+            # Backward pass: copmute the gradients
             loss.backward()
+            # Update the weights using the gradients using adam optimizer
             optimizer.step()
+            # Accumulate the training loss
             running_loss += loss.item()
     avg_trainloss = running_loss / len(trainloader)
     return avg_trainloss
@@ -90,9 +108,12 @@ def test(net, testloader, device):
         for batch in testloader:
             images = batch["img"].to(device)
             labels = batch["label"].to(device)
+            # Get the model prediction
             outputs = net(images)
+            # Compute the loss
             loss += criterion(outputs, labels).item()
+            # Verify how many predictions are correct
             correct += (torch.max(outputs.data, 1)[1] == labels).sum().item()
     accuracy = correct / len(testloader.dataset)
-    loss = loss / len(testloader)
-    return loss, accuracy
+    avg_loss = loss / len(testloader)
+    return avg_loss, accuracy
